d23244bbb36b45c6c7e424d1262b32a078bd6067
jomapormentilla <jomapormentilla@gmail.com>
2023-10-04 02:39:08 -0400

    Added express server folder

---
+++ NodeApp/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'dist/beanstalk-app')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/beanstalk-app/index.html'));
+});
+
+app.listen(port, () => console.log(`Beanstalk app listening on port ${port}!`));
